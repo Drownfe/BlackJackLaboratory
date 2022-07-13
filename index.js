eec2d43fa480523604bc3adeaecc2bafd6a56947
Drownfe <palaciojuanfe@gmail.com>
2022-07-13 18:19:42 -0500

    css styling for first page

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 11
 let sum = firstCard + secondCard
 let blackJack = false
 let message = ""
 let playing = true
+let message1 = document.getElementById("message1")
+console.log(message1)
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "BLACKJACK!!!"
-    blackJack = true
-} else {
-    message = "Sorry, you lost, try again!!!"
-    playing = false
+function gameStart() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "BLACKJACK!!!"
+        blackJack = true
+    } else {
+        message = "Sorry, you lost, try again!!!"
+        playing = false
+    }
+    message1.textContent = message
 }
