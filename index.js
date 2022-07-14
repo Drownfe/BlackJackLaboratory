9774655d21525af9fb6fd798dac01c76f5456276
Drownfe <palaciojuanfe@gmail.com>
2022-07-14 18:13:54 -0500

    html pages organized

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ let player = {
         console.log("Good Mythical Morning!!!")
     }
 }
-
+let cardsArray = [
+    { card: "A" },
+    { card: 2 },
+    { card: 3 },
+    { card: 4 },
+    { card: 5 },
+    { card: 6 },
+    { card: 7 },
+    { card: 8 },
+    { card: 9 },
+    { card: 10 },
+    { card: "J" },
+    { card: "Q" },
+    { card: "K" }
+]
 let cards = []
 let score = 0
 let isBlackJack = false
